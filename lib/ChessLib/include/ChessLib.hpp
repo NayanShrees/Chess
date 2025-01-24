24e7c42bd6d7617d9661709f6acf7e106cbd0640
Nayan Shrees <nayan@nayanshrees.com>
2025-01-24 01:08:59 +0000

    Adding ChessLib
    TODO:
    Actually fix shared lib linking with executable

---
+++ lib/ChessLib/include/ChessLib.hpp
@@ -0,0 +1,11 @@
+#ifndef CHESSLIB_HPP_
+#define CHESSLIB_HPP_
+
+class ChessLib {
+public:
+  ChessLib();
+  ~ChessLib();
+  void PrintBoard();
+};
+
+#endif // CHESSLIB_HPP_
---
+++ lib/ChessLib/src/ChessLib.cpp
@@ -0,0 +1,8 @@
+#include "ChessLib.hpp"
+#include <iostream>
+
+ChessLib::ChessLib() {}
+
+ChessLib::~ChessLib() {}
+
+void ChessLib::PrintBoard() { std::cout << "Hello World" << std::endl; }
